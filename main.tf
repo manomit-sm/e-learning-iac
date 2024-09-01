1e48feddf887617cefb59b8fe6507d871852abf8
Manomit MItra <manomitmitra@Manomits-MacBook-Air.local>
2024-09-01 21:28:52 +0530

    Second commit

--- main.tf
+++ main.tf
@@ -41,6 +41,6 @@ resource "aws_cognito_user_pool_client" "client" {
 }
 
 resource "aws_cognito_user_pool_domain" "cognito-domain" {
-  domain       = "bsolz.net"
+  domain       = "bsolz"
   user_pool_id = "${aws_cognito_user_pool.user_pool.id}"
 }
